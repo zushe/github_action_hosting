3e22e061f244dfadaa45290a749ef9652d68ccde
Zushe Brook <zushe.brook@lseg.com>
2024-11-17 14:40:15 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core');
+
+try {
+  const name = core.getInput('name');
+  const greeting = `Hello, ${name}!`;
+  console.log(greeting);
+  core.setOutput('greeting', greeting);
+} catch (error) {
+  core.setFailed(error.message);
+}
